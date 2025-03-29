361330788d8318ef13e401105312b8e0138ce436
Jan Henkel <35647-janhenkel@users.noreply.gitlab.com>
2025-03-29 01:19:29 +0100

    fix: remove invalid parameter from command creation

--- plugin/open-repo.lua
+++ plugin/open-repo.lua
@@ -10,12 +10,6 @@ vim.api.nvim_create_user_command("OpenRepo", function()
 end, {
     desc = "Open the main repository URL in the configured browser",
     nargs = 0,
-    help = [[
-Opens the main repository page in your configured browser.
-Uses the git remote URL of the current file or working directory to determine the repository.
-For GitHub: Opens https://github.com/owner/repo
-For GitLab: Opens https://gitlab.com/owner/repo
-    ]],
 })
 
 vim.api.nvim_create_user_command("OpenRepoCR", function()
@@ -23,12 +17,6 @@ vim.api.nvim_create_user_command("OpenRepoCR", function()
 end, {
     desc = "Open the change requests URL in the configured browser",
     nargs = 0,
-    help = [[
-Opens the repository's change requests page in your configured browser.
-Uses the git remote URL of the current file or working directory to determine the repository.
-For GitHub: Opens the Pull Requests page (https://github.com/owner/repo/pulls)
-For GitLab: Opens the Merge Requests page (https://gitlab.com/owner/repo/-/merge_requests)
-    ]],
 })
 
 vim.api.nvim_create_user_command("OpenCICD", function()
@@ -36,10 +24,4 @@ vim.api.nvim_create_user_command("OpenCICD", function()
 end, {
     desc = "Open the CI/CD URL in the configured browser",
     nargs = 0,
-    help = [[
-Opens the repository's CI/CD page in your configured browser.
-Uses the git remote URL of the current file or working directory to determine the repository.
-For GitHub: Opens the Actions page (https://github.com/owner/repo/actions)
-For GitLab: Opens the Pipelines page (https://gitlab.com/owner/repo/-/pipelines)
-    ]],
 })
